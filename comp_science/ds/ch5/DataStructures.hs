481b2f4b9de1773442610cbd8840025dcd62ceb8
Isaac Lopez <ilopez@optivon.net>
2022-04-19 12:57:55 -0400

    did exercise 1 from ch5

--- comp_science/ds/ch5/DataStructures.hs
+++ comp_science/ds/ch5/DataStructures.hs
@@ -46,24 +46,40 @@ class Deque q where
     peek :: (Ord a) => q a -> Maybe a
     eject :: (Ord a) => q a -> Maybe (q a)
 
-data DoubleEndedQueue a = DoubleList [a] [a]
+data DoubleEndedQueue a = DoubleList [a] [a] deriving (Show, Eq)
 instance Deque DoubleEndedQueue where
     empty' = DoubleList [] []
     isEmpty' (DoubleList f r) = null f && null r
     
-    cons x (DoubleList [] []) = (DoubleList [x] [])
-    cons x (DoubleList f []) = (DoubleList f [x])
-    cons x (DoubleList [] r) = (DoubleList f' r')
-        where f' = take halfOfR r
-              halfOfR = (length r) `div` 2
-              r' = x : drop halfOfR r
-    cons x (DoubleList f r) = DoubleList f (x:r)
-
+    cons x (DoubleList [] []) = DoubleList [x] []
+    cons x (DoubleList f r) = checkf' (x:f) r
     top' (DoubleList [] _) = Nothing
     top' (DoubleList (x:f) _) = Just x
+    pop' (DoubleList (x:f) r) = case isEmpty' q' of
+        True -> Nothing
+        False -> Just q'
+        where q' = checkf' f r
 
-    pop' (DoubleList [] []) = Nothing
-    pop' (DoubleList _ xs) = Just (DoubleList xs)
+    push' (DoubleList f r) x = checkf' f (x:r)
+    peek (DoubleList _ []) = Nothing
+    peek (DoubleList _ (x:r)) = Just x
+    eject (DoubleList f (x:r)) = case isEmpty' q' of
+        True -> Nothing
+        False -> Just q'
+        where q' = checkf' f r
+    
+checkf' :: (Ord a) => [a] -> [a] -> DoubleEndedQueue a
+checkf' [] r = DoubleList f r''
+    where f = fst $ splitAt half r'
+          r'' = reverse $ snd $ splitAt half r'
+          half = length r `div` 2
+          r' = reverse r
+checkf' f [] = DoubleList f r'
+    where f = fst $ splitAt half f
+          r' = reverse $ snd $ splitAt half f
+          half = length f `div` 2
+checkf' f r = DoubleList f r
 
-checkf' :: (Ord a) => Deque a -> Dequ a
-checkf' 
+fromList' :: (Ord a) => [a] -> DoubleEndedQueue a
+fromList' [] = DoubleList [] []
+fromList' ls = foldl (\acc x -> push' acc x) (DoubleList [] []) ls
